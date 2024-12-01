7145e8da40f643f611f8f2d350fe96a9c553d935
Emmily0 <81269634+Emmily0@users.noreply.github.com>
2024-12-01 08:32:48 +0100

    Added users current location option in HTML and JS

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -26,6 +26,9 @@ zurichTimeElement.innerHTML = zurichTime.format("h:mm:ss [<small>]A[</small>]");
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+}
     let cityName = cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
